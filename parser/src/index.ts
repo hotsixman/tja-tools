a991d4d6d249b45419c535b5de19d6e26cc63a24
HotsixMan <hotsixman05@gmail.com>
2025-12-21 14:46:52 +0900

    fix: main export

--- parser/src/index.ts
+++ parser/src/index.ts
@@ -1,4 +1,12 @@
-export * from './class/Song.js';
+export * from './class/Bar.js';
+export * from './class/BarLine.js';
+export * from './class/Branch.js';
+export * from './class/Command.js';
 export * from './class/Course.js';
+export * from './class/Item.js';
+export * from './class/Song.js';
+
+export * from './exception/ParseException.js';
+export * from './exception/TjaException.js';
 
 export type * from './types.js';
