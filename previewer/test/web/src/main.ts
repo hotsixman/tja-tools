5a9cedef8c829824758f6706416d53d8294246d0
HotsixMan <hotsixman05@gmail.com>
2026-02-27 22:53:15 +0900

    delete: previewer

--- previewer/test/web/src/main.ts
+++
@@ -1,6 +0,0 @@
-import './style.css'
-import sampleTja from './sample.tja?raw';
-import { Song } from 'tja-parser';
-import { Previewer } from '../../../src'
-
-window.previewer = new Previewer(Song.parse(sampleTja), 'edit');
